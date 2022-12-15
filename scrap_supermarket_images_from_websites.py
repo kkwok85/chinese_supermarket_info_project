84a83a3d35e1fa4ded342b18b30fab98bac5ddd6
kkwok85 <kkwok85@gmail.com>
2022-12-15 01:03:36 -0500

    Update scrap_supermarket_images_from_websites.py

--- scrap_supermarket_images_from_websites.py
+++ scrap_supermarket_images_from_websites.py
@@ -39,49 +39,45 @@ urllib.request.urlretrieve("http://feilongmarket.com/images/u12.gif", "local-fil
 
 
 
-
-
-
-htmldata = getdata("https://www.gw-supermarket.com/deal/") 
-
-soup = BeautifulSoup(htmldata, 'lxml') 
-
-
-sidebar = soup.find('div', class_='with-sidebar-wrapper')
-img = sidebar.find_all('img')
-
-
-link[-1]
-for link in soup.find_all('a'):
-    link_p2 = link.get('href')
-
-link_p1 = "http://feilongmarket.com/images/"
-
-    
-urllib.request.urlretrieve(link_p1 + link_p2, "local-filename_test.gif")
-
-urllib.request.urlretrieve("http://feilongmarket.com/images/u12.gif", "local-filename_test.gif")
-
-
-
-
-
+# scrap hmart data
 htmldata = getdata("https://www.hmart.com/weekly-sales-and-events/new-york-new-jersey/") 
 
 soup = BeautifulSoup(htmldata, 'lxml') 
 
 
-sidebar = soup.find('div', class_='img-wrapper')
-img = sidebar.find_all('img')
+#link = soup.find_all('div', class_='img-wrapper')[1]
 
 
-link[-1]
-for link in soup.find_all('a'):
-    link_p2 = link.get('href')
+link_result = []
 
-link_p1 = "http://feilongmarket.com/images/"
+# find all links
+for link in soup.find_all("div", class_ = "img-wrapper"):
+    for img in link.find_all("img", src=True):  # searching for img with src attribute
+        link_result.append(img["src"])
 
-    
-urllib.request.urlretrieve(link_p1 + link_p2, "local-filename_test.gif")
+urllib.request.urlretrieve(link_result[1], "h_mart.jpg")
 
-urllib.request.urlretrieve("http://feilongmarket.com/images/u12.gif", "local-filename_test.gif")
+
+
+
+
+
+# scrap ifresh data
+htmldata = getdata("https://www.ifreshmarket.com/en/flyers/") 
+
+soup = BeautifulSoup(htmldata, 'lxml') 
+
+
+link = soup.find_all('a', class_='waves')
+
+
+link_result = []
+
+# find all links
+for link in soup.find_all("div", class_ = "service-box style-1"):
+    for img in link.find_all("a", class_='waves'):  # searching for img with src attribute
+        link_result.append(img.get('href') )
+
+urllib.request.urlretrieve(link_result[0], "ifresh.jpg")
+
+
